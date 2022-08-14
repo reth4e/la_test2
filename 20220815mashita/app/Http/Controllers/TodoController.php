f33c169ef50f554e92c49470fb25d8128c9a066e
reth4e <anmnaic@gmail.com>
2022-08-14 18:24:31 +0900

    0814

---
+++ 20220815mashita/app/Http/Controllers/TodoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+use App\Http\Requests\TodoRequest;
+
+class TodoController extends Controller
+{
+    
+}
---
+++ 20220815mashita/app/Models/Todo.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['content'];
+
+    public function user(){
+		return $this->belongsTo('App\Models\User');
+    }
+
+    public function tag(){
+		return $this->belongsTo('App\Models\Tag');
+    }
+}
